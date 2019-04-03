df6ab90433ff02ec0a427df751dbff5087fd02f5
Simon Vandel Sillesen <simon.vandel@gmail.com>
2019-04-03 13:23:19 +0200

    fix bit_search zero_allocate

--- src/main.rs
+++ src/main.rs
@@ -16,24 +16,17 @@ use rayon::prelude::*;
 //    slice: &'data [T],
 //}
 
-
 fn main() {}
 
-fn bit_search<'a>(needle: &'a BitSlice, haystack: &'a BitSlice) -> impl Iterator<Item=&'a usize> {
-//    let mut result: Vec<_> = vec![];
-//    for (index, window) in haystack.windows(needle.len()).enumerate() {
-//        if window == needle {
-//            result.push(index);
-//        }
-//    }
-
-    haystack.windows(needle.len())
+fn bit_search<'a>(
+    needle: &'a BitSlice,
+    haystack: &'a BitSlice,
+) -> impl Iterator<Item = usize> + 'a {
+    haystack
+        .windows(needle.len())
         .enumerate()
-        .filter(move |(index, window)| *window == needle)
-        .map(|(index, _)| &
-            index)
-
-    //return result;
+        .filter(move |(_, window)| *window == needle)
+        .map(|(index, _)| index)
 }
 
 #[cfg(test)]
@@ -42,12 +35,11 @@ mod tests {
 
     #[test]
     fn test_search() {
-        let haystack = bitvec![1,1,1,1,1,0,0,0,1,0,0];
-        let needle = bitvec![1,0,0];
+        let haystack = bitvec![1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0];
+        let needle = bitvec![1, 0, 0];
 
-        let result: Vec<&usize> = bit_search(&needle, &haystack).collect();
+        let result: Vec<usize> = bit_search(&needle, &haystack).collect();
 
-
-        assert_eq!(result, vec![&4, &8]);
+        assert_eq!(result, vec![4, 8]);
     }
 }
